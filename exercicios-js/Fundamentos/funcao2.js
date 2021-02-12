59c06b8c15729c306855f54978ae8fe386efba87
matheusnunes123 <matheusnunesquarto@gmail.com>
2021-02-12 15:21:57 -0300

    usando let e var

---
+++ exercicios-js/Fundamentos/funcao2.js
@@ -0,0 +1,24 @@
+// armazenando uma função em uma váriavel
+const imprimirSoma = function (a,b){
+    console.log(a + b)
+}
+
+imprimirSoma(2,3)
+
+//Armazenando uma funcao arrow em uma variavel
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 3))
+
+//retorno implícito
+
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(2,3))
+
+
+const imprimir2 = a => console.log(a)
+
+imprimir2('Legal!!')
