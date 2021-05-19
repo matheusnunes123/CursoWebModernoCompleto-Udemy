a335d559662a31bad0a14b5bcf6a4c19099ca4bb
matheusnunes123 <matheusnunesquarto@gmail.com>
2021-05-19 18:04:02 -0300

    Objects

--- exercicios-js/Objeto/objetoRevisao.js
+++ exercicios-js/Objeto/objetoRevisao.js
@@ -1,4 +1,47 @@
 // coleção dinâmica de pares chave/valor
 const produto = new Object
 produto.nome = 'Cadeira'
-prduto['Marca do produto'] = 'Generica'
+produto['Marca do produto'] = 'Generica'
+produto.preco = 220
+
+console.log(produto)
+delete produto.preco
+delete produto['Marca do produto']
+
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logradouro: 'Rua abc',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    },{
+        nome:'Ana',
+        idade: 42
+    }],
+    calcularValorSeguro: function(){
+        // ...
+    }
+}
+
+carro.proprietario.endereco.numero = 500
+carro['proprietario']['endereco']['logradouro'] = 'Rua gigante'
+
+console.log(carro) 
+
+
+// delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+console.log(carro) 
+console.log(carro.condutores)
+console.log(carro.condutores.length)
