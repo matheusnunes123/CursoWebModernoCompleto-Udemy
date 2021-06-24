e22888bc64f8c35b9ad5bcedbb165ae59ff77de6
matheusnunes123 <matheusnunesquarto@gmail.com>
2021-06-24 13:51:03 -0300

    ForEach

--- exercicios-js/Array/foreach1.js
+++ exercicios-js/Array/foreach1.js
@@ -3,3 +3,8 @@ const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 aprovados.forEach(function(nome, indice){
     console.log(`${indice + 1}) ${nome}`)
 })
+
+aprovados.forEach( nome => console.log(nome))
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovados.forEach(exibirAprovados)
